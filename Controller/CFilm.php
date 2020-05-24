2481d75493d48040dc9b4a1e5a02155286c4965f
Alessio <alessiobb3b@gmail.com>
2020-05-24 21:46:37 +0200

    inseriti nei film i consigliati in base al genere

--- Controller/CFilm.php
+++ Controller/CFilm.php
@@ -9,8 +9,23 @@ class CFilm
         $filmID = $_GET["film"];
         $autoplay = isset($_GET["autoplay"]) && $_GET["autoplay"];
         $film = $pm->load($filmID, "id", "EFilm")[0];
+        $filmC = $pm->load($film->getGenere(),"Genere","EFilm");
+        $locandine = [];
+        foreach($filmC as $key => $f) {
+            if ($f->getId() == $filmID) {
+                echo '<br>'. $filmID . " " . $key . '<br>';
+                unset($filmC[$key]);
+            }
+        }
+        $filmC = array_values($filmC);
+        if(sizeof($filmC) > 6){
+            $filmC = array_slice($filmC,0,6);
+        }
         $img = $pm->load($filmID,"idFilm","EMediaLocandina");
+        foreach($filmC as $loc) {
+            array_push($locandine,$pm->load($loc->getId(),"idFilm","EMediaLocandina"));
+        }
         $vfilm = new VFilm();
-        $vfilm->show($film, $autoplay, $img);
+        $vfilm->show($film, $autoplay, $img, $filmC, $locandine);
     }
 }
--- View/VFilm.php
+++ View/VFilm.php
@@ -10,11 +10,13 @@ class VFilm
         $this->smarty = StartSmarty::configuration();
     }
 
-    public function show(EFilm $film, bool $autoplay, EMedia $img)
+    public function show(EFilm $film, bool $autoplay, EMedia $img, array $filmconsigliati, array $imgconsigliati)
     {
         $this->smarty->assign("film", $film);
+        $this->smarty->assign("consigli", $filmconsigliati);
         $this->smarty->assign("autoplay", $autoplay);
         $this->smarty->assign("locandina", $img);
+        $this->smarty->assign("immagini", $imgconsigliati);
         $this->smarty->assign("registi", $film->getRegisti());
         $this->smarty->assign("attori", $film->getAttori());
         $this->smarty->display("film.tpl");
