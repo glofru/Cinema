9c7ee1ee823095ed62d4f2e8f48f5eb9f817970a
Alessio <alessiobb3b@gmail.com>
2020-05-24 18:51:33 +0200

    modificato EHelper per le date

--- Controller/CHome.php
+++ Controller/CHome.php
@@ -5,17 +5,15 @@ class CHome
 
     public static function showHome() {
         $pm = FPersistentManager::getInstance();
-        $oggi = new DateTime('now');
-        $oggi = $oggi->format('Y-m-d');
-        $filmProssimi = $pm->loadBetween($oggi,'2100-01-01',"EFilm");
+        $gestore = EHelper::getInstance();
+        $date = $gestore->getDateProssime();
+        $filmProssimi = $pm->loadBetween($date[0],$date[1],"EFilm");
         $immaginiProssimi = [];
         foreach($filmProssimi as $film){
-            array_push($immaginiProssimi,$pm->load($film->getId(),"idFilm","EMediaLocandina"));
+            array_push($immaginiProssimi,$pm->load($film->getId(),"idFilm","EMedia"));
         }
-        $oggi = new DateTime('tomorrow');
-        $week = new DateInterval('P6D');
-        $fine = $oggi->add($week);
-        $temp = $pm->loadBetween($oggi->format('Y-m-d'),$fine->format('Y-m-d'),"EProiezione");
+        $date = $gestore->getSettimana()
+        $temp = $pm->loadBetween($date[0],$date[1],"EProiezione");
         $filmProiezioni = [];
         $immaginiProiezioni = [];
         foreach($temp as $pro){
@@ -24,16 +22,15 @@ class CHome
                 if($pro->getFilm()->getNome() == $film->getNome())
                     $found = true;
             }
-            if(!$found)
+            if(!$found)        
                 array_push($filmProiezioni,$pro->getFilm());
         }
         foreach($filmProiezioni as $film){
             array_push($immaginiProiezioni,$pm->load($film->getId(),"idFilm","EMedia"));
         }
         $pm = FPersistentManager::getInstance();
-        $oggi = new DateTime('first day of this month - 2 weeks');
-        $oggi = $oggi->format('Y-m-d');
-        $filmConsigliati = $pm->loadBetween("0000-00-00",$oggi,"EFilm");
+        $date = $gestore->getDatePassate();
+        $filmConsigliati = $pm->loadBetween($date[0],$date[1],"EFilm");
         $immaginiConsigliati = [];
         foreach($filmConsigliati as $film){
             array_push($immaginiConsigliati,$pm->load($film->getId(),"idFilm","EMedia"));
---
+++ Model/EUtility/EHelper.php
@@ -0,0 +1,89 @@
+<?php
+
+
+class EHelper
+{
+    private static $instance;
+
+    private function __construct() {}
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new EHelper();
+        }
+        return self::$instance;
+    }
+
+    public function username(string $username): bool {
+        $res = preg_replace("/[^a-zA-Z]/", "", $username);
+        if(strlen($username) < 8 || $res !== $username ) {
+            return false;
+        }
+        return true;
+    }
+
+    public function password(string $password): bool {
+        if(strlen($password) < 8) {
+            return false;
+        }
+        return true;
+    }
+
+    public function email(string $email): bool {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+        return true;
+    }
+
+    public function date(string $date) {
+        $elem = explode("-",$date);
+        if(sizeof($elem) !== 3) {
+            return false;
+        }
+        return checkdate($elem[1],$elem[2],$elem[0]);
+    }
+
+    public function hour(string $hour) {
+        $res = strtotime($hour);
+        if($res !== false){
+            return true;
+        }
+        return $res;
+    }
+
+    public function getDateProssime(): array {
+        $result = [];
+        $oggi = new DateTime('now');
+        $oggi = $oggi->format('Y-m-d');
+        $fine = "2100-01-01";
+        array_push($result,$oggi,$fine);
+        return $result;
+    }
+
+    public function getSettimana(): array {
+        $result = [];
+        $inizio = new DateTime('tomorrow');
+        $giorno = $inizio->format('D');
+        while($giorno != 'Mon') {
+            $inizio->modify('-1 Day');
+            $giorno = $inizio->format('D');
+        }
+        $inizio = $inizio->format('Y-m-d');
+        $fine = DateTime::createfromFormat('Y-m-d',$inizio);
+        $fine->modify('+ 6 Days');
+        $fine = $fine->format('Y-m-d');
+        array_push($result,$inizio,$fine);
+        return $result;
+    }
+
+    public function getDatePassate():array {
+        $oggi = new DateTime('first day of this month - 2 weeks');
+        $oggi->format('Y-m-d');
+        $fine = new DateTime('first day of this month - 6 months');
+        $fine = $fine->format('Y-m-d');
+        $date = [];
+        array_push($date,$oggi,$fine);
+        return $date;
+    }
+}
