7e01fd2e8a7eac1edacd96b78fce080a5c38bd7f
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2020-06-02 20:09:33 +0200

    [WIP] Acquisto

--- Controller/CAcquisto.php
+++ Controller/CAcquisto.php
@@ -11,28 +11,7 @@ class CAcquisto
             if (!isset($id) || !isset($str)) {
                 VError::error(8);
             } elseif (CUtente::isLogged()) { //Utente registrato
-                $pm = FPersistentManager::getInstance();
-
-                $posti = EPosto::fromString($str, true);
-                $proiezione = $pm->load($id, "id", "EProiezione")->getElencoProgrammazioni()[0]->getProiezioni()[0];
-                $locandina = $pm->load($proiezione->getFilm()->getId(), "idFilm", "EMedia");
-                $utente = CUtente::getUtente();
-                $biglietti = [];
-                $totale = 0;
-
-                foreach ($posti as $key => $posto) {
-                    $costo = EBiglietto::getPrezzofromProiezione($proiezione);
-                    array_push($biglietti, new EBiglietto($proiezione, $posto, $utente, $costo, uniqid()));
-                    $totale += $costo;
-                }
-
-                if(sizeof($biglietti) > 0) {
-                   $serialized = serialize($biglietti);
-                   $_SESSION["biglietti"] = $serialized;
-                   VAcquisto::showAcquisto($biglietti, $locandina, $utente, $totale);
-                } else {
-                    VError::error(8);
-                }
+                self::loadBiglietti($id, $str);
             } elseif (isset($_POST["mail"]) && EInputChecker::getInstance()->isEmail($_POST["mail"])) { //Utente non registrato
                 $mail = $_POST["mail"];
 
@@ -41,7 +20,9 @@ class CAcquisto
                 if ($utente != null && $utente->isRegistrato()) {
                     VUtente::loginForm($mail, false);
                 } else {
-                    die;
+                    session_start();
+                    $_SESSION["nonRegistrato"] = serialize($utente);
+                    self::loadBiglietti($id, $str);
                 }
             } else { //Errore, l'utente non è loggato e non ha inviato la mail, non dovrebbe accadere
                 VError::error(8);
@@ -49,6 +30,31 @@ class CAcquisto
 
         } else {
             CMain::notFound();
+        }
+    }
+
+    private static function loadBiglietti(int $id, string $str) {
+        $pm = FPersistentManager::getInstance();
+
+        $posti = EPosto::fromString($str, true);
+        $proiezione = $pm->load($id, "id", "EProiezione")->getElencoProgrammazioni()[0]->getProiezioni()[0];
+        $locandina = $pm->load($proiezione->getFilm()->getId(), "idFilm", "EMedia");
+        $utente = CUtente::getUtente();
+        $biglietti = [];
+        $totale = 0;
+
+        foreach ($posti as $key => $posto) {
+            $costo = EBiglietto::getPrezzofromProiezione($proiezione);
+            array_push($biglietti, new EBiglietto($proiezione, $posto, $utente, $costo, uniqid()));
+            $totale += $costo;
+        }
+
+        if(sizeof($biglietti) > 0) {
+            $serialized = serialize($biglietti);
+            $_SESSION["biglietti"] = $serialized;
+            VAcquisto::showAcquisto($biglietti, $locandina, $utente, $totale);
+        } else {
+            VError::error(8);
         }
     }
 
